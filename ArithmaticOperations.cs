2a7e9f349a17278de93ed8bd2e24809bc9460dc9
santhossundar <santhoshsunthar@gmail.com>
2023-03-23 23:06:40 +0530

    Added arithmatic operations

---
+++ ArithmaticOperations.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpProgramming
+{
+    internal class ArithmaticOperations
+    {
+        public ArithmaticOperations()
+        {
+            // Addition
+            int payment = 1000;
+            int tax = 50;
+            int total = payment * tax;
+            Console.WriteLine(total);
+
+            // Substraction
+            int discount = 100;
+            int balance = total - discount;
+            Console.WriteLine(balance);
+
+            // Multiplication
+            int lenght = 30;
+            int height = 60;
+            int square = lenght * height;
+            Console.WriteLine(square);
+
+            // Divition
+            int noOfItems = 5;
+            int priceForEachItem = total / noOfItems;
+            Console.WriteLine(priceForEachItem);
+        }
+    }
+}
