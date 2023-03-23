792427cc364cc037ece23c629d0cb152eaed55fb
santhossundar <santhoshsunthar@gmail.com>
2023-03-23 22:25:23 +0530

    Data Types

---
+++ DataTypes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    internal class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            int intNumber = 10; 
+            Console.WriteLine(intNumber);
+
+            long longNumber = 1234567890;
+            Console.WriteLine(longNumber);
+
+            float floatNumber = 3.9f;
+            Console.WriteLine(floatNumber);
+
+            double doubleNumber = 5.1234567890;
+            Console.WriteLine(doubleNumber);
+
+            char hash = '#';
+            Console.WriteLine(hash);
+
+            string myFullName = "Santhos Suntharalingam";
+            Console.WriteLine(myFullName);
+        }
+    }
+}
